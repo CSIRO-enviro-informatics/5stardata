5baec38548935b8f32de894c88d35e4a15216c4c
Jonathan Yu <jonathan.yu@csiro.au>
2016-11-09 16:13:52 +1100

    Adding initial handling for charts

--- js/5-star-oznome-data.js
+++ js/5-star-oznome-data.js
@@ -360,5 +360,58 @@ function sendDataToServer(survey) {
   var resultAsString = JSON.stringify(survey.data);
   
   $( ".survey-results pre").text(resultAsString)
+  var data = processSurveyData(survey.data);
+  var ctx = $( ".survey-results canvas#surveyChart");
+  updateSurveyChart(ctx, data);
+  
   //alert(resultAsString); //send Ajax request to your web server.
 };
+
+var processSurveyData = function(data) {
+	var p1 = []
+	var p2 = {
+		labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+		datasets: [
+			{
+				label: "My First dataset",
+				backgroundColor: "rgba(179,181,198,0.2)",
+				borderColor: "rgba(179,181,198,1)",
+				pointBackgroundColor: "rgba(179,181,198,1)",
+				pointBorderColor: "#fff",
+				pointHoverBackgroundColor: "#fff",
+				pointHoverBorderColor: "rgba(179,181,198,1)",
+				data: [65, 59, 90, 81, 56, 55, 40]
+			},
+			{
+				label: "My Second dataset",
+				backgroundColor: "rgba(255,99,132,0.2)",
+				borderColor: "rgba(255,99,132,1)",
+				pointBackgroundColor: "rgba(255,99,132,1)",
+				pointBorderColor: "#fff",
+				pointHoverBackgroundColor: "#fff",
+				pointHoverBorderColor: "rgba(255,99,132,1)",
+				data: [28, 48, 40, 19, 96, 27, 100]
+			}
+		]
+	};
+	
+	return p2;
+}
+
+var updateSurveyChart = function(ctx, data) {
+	var options = {
+            scale: {
+                ticks: {
+                    beginAtZero: true
+                }
+            }
+	
+	var myRadarChart = new Chart(ctx, {
+		type: 'radar',
+		data: data,
+		options: options
+	});
+	
+	
+	return myRadarChart;
+}
