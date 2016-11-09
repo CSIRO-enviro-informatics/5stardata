9a41664cbd03f89d902531f47f244960c7e85029
Jonathan Yu <jonathan.yu@csiro.au>
2016-11-09 15:47:01 +1100

    Updating questions

--- js/5-star-oznome-data.js
+++ js/5-star-oznome-data.js
@@ -1,17 +1,351 @@
-var surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-  { name:"page1", questions: [ 
-      { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-      { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visible: false }
-   ]},
-  { name: "page2", questions: [
-    { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-    { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visible: false } ] },
-  { name: "page3",questions: [
-    { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
+var surveyJSON = {
+ pages: [
+  {
+   name: "page1",
+   questions: [
+    {
+     type: "multipletext",
+     items: [
+      {
+       name: "dataset-name",
+       title: "Name"
+      },
+      {
+       name: "dataset-url",
+       title: "URL"
+      }
+     ],
+     name: "question1",
+     title: "Enter dataset details"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      {
+       value: "no",
+       text: "No"
+      },
+      {
+       value: "sneaker-net",
+       text: "Sneaker Net"
+      },
+      {
+       value: "data-download",
+       text: "Data download"
+      },
+      {
+       value: "web-service",
+       text: "Web service (bespoke)"
+      },
+      {
+       value: "standard-web-service",
+       text: "Web service (standardised - e.g. OGC)"
+      }
+     ],
+     name: "question2",
+     title: "Is the data accessible to users other than the creator or owner?"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question3",
+     title: "described and tagged, indexed in a well known system (can be general purpose or community specific) "
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question4",
+     title: "Denoted using a stable, persistent identifier"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question5",
+     title: "represented using a common or community-endorsed (i.e. standard) format - loadable"
+    }]
+  },	
+  {
+   name: "page2",
+   questions: [	
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question6",
+     title: "structured using a discoverable, community-endorsed (standard?) schema or data model - useable"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question7",
+     title: "Accessible using a well-described API - queriable, sub-settable"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question8",
+     title: "REST API - web application-compatible"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question9",
+     title: "internal elements (e.g. column definitions, units of measure) are linked to accessible (standard) definitions - comprehensible "
+    }]
+  },
+  {
+   name: "page4",
+   questions: [
+	
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question10",
+     title: "Linked using externally defined identifiers (e.g. URIs) - crawlable"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question11",
+     title: "Part of a regular data collection program or series, with a clear maintenance and update schedule"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question12",
+     title: "Data-quality assessment available"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question13",
+     title: "Origin and workflow that produced the data is available - provenance"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question14",
+     title: "Conditions for re-use are available and clearly expressed - licensed "
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      "one",
+      {
+       value: "two",
+       text: "second value"
+      },
+      {
+       value: "three",
+       text: "third value"
+      }
+     ],
+     name: "question15",
+     title: "Information about how the data has been used, and how many times"
+    }
+   ],
+   title: "Dataset characteristics"
+  },
+  {
+   name: "page5",
+   questions: [
+    {
+     type: "radiogroup",
+     choices: [
+      "low",
+      {
+       value: "med",
+       text: "medium"
+      },
+      "high"
+     ],
+     isRequired: true,
+     name: "mvvmUsing",
+     title: "Complexity of the project"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      {
+       value: "1",
+       text: "1 discipline"
+      },
+      {
+       value: "2-4",
+       text: "2-4 disciplines"
+      },
+      {
+       value: "5",
+       text: "5 or more disciplines"
+      }
+     ],
+     hasOther: true,
+     isRequired: true,
+     name: "mvvm",
+     title: "Interdisciplinary project?"
+    },
+    {
+     type: "radiogroup",
+     choices: [
+      {
+       value: "1",
+       text: "1 organisation"
+      },
+      {
+       value: "2",
+       text: "2-4 organisations"
+      },
+      {
+       value: "3",
+       text: "5 or more organisations"
+      }
+     ],
+     name: "question16",
+     title: "Cross-organisational project?"
+    }
+   ],
+   title: "Project, organisational, institutional"
+  }
  ],
+ title: "Tell us about your data",
  triggers: [
-  { type: "visible", operator: "equal", value: "Yes", name: "frameworkUsing", questions: ["framework"]},
-  { type: "visible", operator: "equal", value: "Yes", name: "mvvmUsing", questions: ["mvvm"]}
+  {
+   type: "visible",
+   operator: "equal",
+   value: "Yes",
+   name: "frameworkUsing",
+   questions: [
+    "framework"
+   ]
+  },
+  {
+   type: "visible",
+   operator: "equal",
+   value: "Yes",
+   name: "mvvmUsing",
+   questions: [
+    "mvvm"
+   ]
+  }
  ]
 }
 
