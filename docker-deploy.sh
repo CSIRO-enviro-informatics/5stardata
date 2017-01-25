5bd66302b8201b2ddb1893e002807efd0d2540d3
lw-11-mel <root@lw-11-mel.nexus.csiro.au>
2017-01-25 13:55:19 +1100

    security fixes for apache

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,1 +1,1 @@
-docker run -dit --name oznome-5star -p 12000:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
+docker run -dit --name oznome-5star-prod -p 12000:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4.25
